cc7c70002cf5ef9957e6cb8113e07708f61a0074
vladzur <vladzur@gmail.com>
2015-11-02 10:42:12 -0300

    GPL3 license

--- src/FormBuilder.php
+++ src/FormBuilder.php
@@ -1,8 +1,21 @@
 <?php
 /**
- * Created by vladzur.
- * Date: 29-09-15
- * Time: 07:14 PM
+ * Kogin <vladzur@gmail.com>
+ * Copyright (c) 2015 Vladimir Zurita <vladzur@gmail.com>
+ *
+ * This file is part of Kogin.
+ * Kogin is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 namespace Vladzur\Kogin;
